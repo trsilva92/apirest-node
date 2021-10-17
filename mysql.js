6238e5270805ecdeba734651ce1ed2de489811ae
Tiago Ribeiro da Silva <tiagors1992@gmail.com>
2021-10-17 18:35:33 -0300

    conectando a API ao banco de dados MYSQL

--- mysql.js
+++ mysql.js
@@ -1,12 +1,11 @@
-const mysql = require('mysql')
-require('./nodemon.json').config
+const mysql = require('mysql2')
 
 const pool = mysql.createPool({
-    "user": process.env.MYSQL_USER,
-    "password": process.env.PASSWORD,
-    "database": process.env.MYSQL_DATABASE,
-    "host": process.env.MYSQL_HOST,
-    "port": process.env.MYSQL_PORT
+    "user": "root",
+    "password": "root",
+    "database": "ecommerce",
+    "host": "localhost",
+    "port": "3306"
 })
 
 exports.pool = pool
--- routes/produtos.js
+++ routes/produtos.js
@@ -3,29 +3,36 @@ const router = express.Router()
 const mysql = require('../mysql').pool
 
 // Retorna todos os produtos
-router.get('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: "Retornando consulta de todos os produtos"
+router.get('/', (req, res) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: "error" }) }
+        conn.query(
+            'SELECT * FROM produtos',
+            (error, resultado, field) => {
+                conn.release()
+
+                if (error) {
+                    return res.status(500).send({ mensagem: "Não foi retornado nenhum produto na consulta" })
+                }
+                return res.status(200).send({ response: resultado })
+            })
     })
 })
 
 // Insere um produto
-router.post('/', (req, res, next) => {
-    
+router.post('/', (req, res) => {
     mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: "error" }) }
         conn.query(
-            'insert into produtos (nome, preco) values (?, ?)', 
+            'INSERT INTO produtos (nome, preco) VALUES (?, ?)',
             [req.body.nome, req.body.preco],
             (error, resultado, field) => {
                 conn.release()
 
                 if (error) {
-                    return res.status(500).send({
-                        error: error,
-                        response: null
-                    })
+                    return res.status(500).send({ mensagem: "Não foi retornado nenhum produto na consulta" })
                 }
-                
+
                 res.status(201).send({
                     mensagem: "Produto inserido com sucesso",
                     id_produto: resultado.insertId
@@ -36,30 +43,37 @@ router.post('/', (req, res, next) => {
 })
 
 // Retorna os dados de um produto específico
-router.get('/:id_produto', (req, res, next) => {
-    const id = req.params.id_produto
-
-    if (id === 'especial') {
-        res.status(200).send({
-            mensagem: "você descobriu o id especial",
-            id: id
-        })
-    } else {
-        res.status(200).send({
-            mensagem: "você passou um ID"
-        })
-    }
-
-    res.status(200).send({
-        mensagem: "usando o GET de um produto específico",
-        id: id
+router.get('/:id_produto', (req, res) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: "error" }) }
+        conn.query(
+            'SELECT * FROM produtos WHERE id_produto = ?;',
+            [req.params.id_produto],
+            (error, resultado, field) => {
+                if (error) {
+                    return res.status(500).send({ error: "error" })
+                }
+                return res.status(200).send({ response: resultado })
+            })
     })
 })
 
 // Altera um produto
-router.patch('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: "Produto alterado com sucesso"
+router.patch('/', (req, res) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: "error" }) }
+        conn.query(
+            `UPDATE produtos 
+            SET nome = ?
+            WHERE id_produto = ?;`,
+            [req.body.nome, req.body.id_produto],
+            (error, resultado, field) => {
+
+                if (error) {
+                    return res.status(500).send({ error: "deu ruim hein" })
+                }
+                return res.status(200).send({ mensagem: "produto alterado com sucesso" })
+            })
     })
 })
 
